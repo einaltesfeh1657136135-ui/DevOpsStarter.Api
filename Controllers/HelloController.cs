b81d1f5b084ea3d0a861b9615e507d41e8ab0e37
mengxu <einaltesfeh1657136135@gmail.com>
2025-12-17 20:15:54 +0100

    Add logging and configuration with environment override

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -7,10 +7,24 @@ namespace DevOpsStarter.Api.Controllers
     [ApiController]
     public class HelloController : ControllerBase
     {
+        private readonly ILogger<HelloController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public HelloController(
+            ILogger<HelloController> logger,
+            IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Hello DevOps");
+            var appName = _configuration["App:Name"];
+            var env = _configuration["App:Environment"];
+            _logger.LogInformation("Hello endpoing called form {Environment}", env);
+
+            return Ok($"Hello from {appName} ({env})");
         }
     }
 }
